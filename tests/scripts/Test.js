3d29eb7959c3bab0dd4cf5ee0e7662a753fd4154
Faidon Dermesonoglou <faidon.dermesonoglou@gmail.com>
2018-12-17 15:52:11 +0000

    add tests folder

---
+++ tests/scripts/Test.js
@@ -0,0 +1,362 @@
+
+///////////
+
+MyDateTimeFix = function() {
+    this.dd = "";
+};
+
+MyDateTimeFix.prototype.someFileAdjs = function(DateTimeStr) {
+    try {
+        var tempDD = DateTimeStr;
+        var helpY = parseInt(tempDD.substr(0,4));var helpM = parseInt(tempDD.substr(5,2));            
+        var helpD = parseInt(tempDD.substr(8,2));var helpH = parseInt(tempDD.substr(11,2));
+                
+        var ddate = new Date(helpY,helpM-1,helpD,helpH,0,0);
+            
+        if ((ddate instanceof Date) && !isNaN(ddate)) {
+            this.dd = '[object Date]';            
+        }
+    }
+    catch(err) {
+        alert("Error creating Date");
+    }
+};
+
+///////////
+
+function arrCountr(myCountry) {
+    var myCFData =[];
+    
+    var d1 = {
+        Day: 1,
+        Hour: 17,
+        InMapCode: "CZ",
+        Month: 9,
+        OutMapCode: "PL",
+        Quantity: 0,
+        Service: "Co",
+        Type: "I",
+        Year: 2018
+    };
+        
+    var d2 = {
+        Day: 2,
+        Hour: 15,
+        InMapCode: "CZ",
+        Month: 9,
+        OutMapCode: "DE",
+        Quantity: -477,
+        Service: "Co",
+        Type: "I",
+        Year: 2018
+    };
+        
+    var d3 = {
+        Day: 1,
+        Hour: 10,
+        InMapCode: "CZ",
+        Month: 9,
+        OutMapCode: "SK",
+        Quantity: -674.2,
+        Service: "Co",
+        Type: "I",
+        Year: 2018
+    }; 
+        
+    var d4 = {
+        Day: 12,
+        Hour: 4,
+        InMapCode: "DE",
+        Month: 8,
+        OutMapCode: "CZ",
+        Quantity: 298,
+        Service: "Co",
+        Type: "E",
+        Year: 2018
+    };          
+    
+    var d5 = {
+        Day: 3,
+        Hour: 4,
+        InMapCode: "CZ",
+        Month: 9,
+        OutMapCode: "AT",
+        Quantity: -96,
+        Service: "Co",
+        Type: "I",
+        Year: 2018
+    };    
+    
+    myCFData.push(d1,d2,d3,d4,d5);
+    
+    var arrayCountry = [];
+    arrayCountry = myCFData.map(function(d) {
+       return d.OutMapCode;
+    });    
+    arrayCountry.splice(arrayCountry.indexOf(myCountry), 1 );
+    
+    return arrayCountry;
+} 
+
+///////////
+
+function p_composite_chart(mapCode) {
+
+    var dimension=[{
+        Day: 15,
+        Hour: 1,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 60.09,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 2,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 51.09,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 3,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 49,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 4,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 46.27,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 5,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 45.37,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 6,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 50.14,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 7,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 54.83,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 8,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 73.1,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 9,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 78.17,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 10,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 77.9,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 11,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 75.51,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 12,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 75,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 13,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 72,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 14,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 70.01,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 15,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 68.76,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 16,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 70.08,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 17,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 73.03,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 18,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 75.93,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 19,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 81.91,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 20,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 97,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 21,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 96.18,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 22,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 81,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 23,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 73.89,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018},{
+        Day: 15,
+        Hour: 24,
+        InMapCode: "SK",
+        Month: 9,
+        OutMapCode: "SK",
+        Price: 59.93,
+        Quantity: NaN,
+        Service: "Pr",
+        Type: "E",
+        Year: 2018}];
+
+        function prices_by_day (dimension, mapCode) {
+            p = { count: 0, total: 0, average: 0};
+        
+            dimension.forEach(function(v){
+                if(v.OutMapCode == mapCode && v.Service == "Pr") { 
+                    p.count++;
+                    p.total+= parseFloat(v.Price);
+                    p.average = parseFloat(p.total / p.count);
+                }            
+            });    
+        
+            return p; 
+        }
+    
+        return prices_by_day(dimension, mapCode);
+}
---
+++ tests/spec/TestSpec.js
@@ -0,0 +1,64 @@
+
+///////////
+
+describe("MyDateTimeFix string param to Date result format", function() {
+    beforeEach(function() {
+        mDTF = new MyDateTimeFix;
+    });
+
+    describe("Check Date Type function", function() {
+        it("should return type object date when day is two digit 31", function() {
+            mDTF.someFileAdjs("2018-07-31 22:00:00.000");
+            expect(mDTF.dd).toBe('[object Date]');
+        });
+        
+        it("should return type object date when day is one digit 9", function() {
+            mDTF.someFileAdjs("2018-09-09 14:00:00.000");
+            expect(mDTF.dd).toBe('[object Date]');
+        });        
+        
+        it("should return an error if param is a Date Type, no substr func can work"
+        , function() { 
+            spyOn(window, "alert");
+            mDTF.someFileAdjs(new Date("2018-07-31 22:00:00.000"));
+            expect(window.alert).toHaveBeenCalledWith("Error creating Date");
+        });        
+        
+    });
+});
+
+///////////
+
+describe("arrCountry custom function map showing neigbouring countries", function() {
+    beforeEach(function() {
+        this.result = new arrCountr("CZ"); 
+     });
+
+    it("should return true as it is the main hub not neighbour", function() {
+        expect(this.result).not.toEqual(jasmine.objectContaining(["CZ"]));
+    });
+
+    it("should return 1 failure spec as it is a neighbour not main hub", function() {
+        expect(this.result).not.toEqual(jasmine.objectContaining(["PL"]));
+    });
+
+    it("should return true all main Hub neighbours", function() {
+        expect(this.result).toEqual(["PL", "DE", "SK", "AT"]);
+    });
+});
+
+///////////
+
+describe("show_day_P_composite_chart custom reduce function Price of SK Month 9 day 15", function() {
+    beforeEach(function() {
+        this.value = new p_composite_chart("SK");
+    });
+    
+    it("should return true that 24 hours are counted. should return true that the average of SK for that day equals the data title of the chart", function() {
+        expect(this.value.count).toEqual(24);
+        expect(this.value.average).toEqual(69.00791666666669);
+    });    
+    
+});
+
+///////////
